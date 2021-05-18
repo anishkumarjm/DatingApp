68c83c83f83c2547e2597b8b8f31670c96719b70
anishkumarjm <anishkumarjm@gmail.com>
2021-05-18 21:30:06 +0530

    End of Section 8

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
+using API.DTOs;
 using API.Entities;
+using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -11,23 +14,31 @@ using Microsoft.Extensions.Logging;
 
 namespace API.Controllers
 {
+    [Authorize]
     public class UsersController : BaseApiController
     {
-        
-        private readonly DataContext _context;
-        public UsersController(DataContext context)
+
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+
+        public UsersController(IUserRepository userRepository, IMapper mapper)
         {
-            _context = context;            
+            _mapper = mapper;
+            _userRepository = userRepository;
+
         }
-        [AllowAnonymous]
+
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers(){
-            return await _context.Users.ToListAsync();
+        public async Task<ActionResult<IEnumerable<MemberDTO>>> GetUsers()
+        {
+            var users = await _userRepository.GetMembersAsync();
+            return Ok(users);
         }
-          [Authorize]
-          [HttpGet("{id}")]
-         public ActionResult<AppUser> GetUser(int id){
-         return _context.Users.Find(id);
+
+        [HttpGet("{username}")]
+        public async Task<ActionResult<MemberDTO>> GetUser(string username)
+        {
+            return await _userRepository.GetMemberAsync(username);            
         }
     }
 }
--- API/DTOs/MemberDTO.cs
+++ API/DTOs/MemberDTO.cs
@@ -1,17 +1,15 @@
-
 using System;
 using System.Collections.Generic;
-using API.Extentions;
+using API.Entities;
 
-namespace API.Entities
+namespace API.DTOs
 {
-    public class AppUser
+    public class MemberDTO
     {
-        public int Id { get; set; }
-        public string UserName { get; set; }       
-        public byte[] PasswordHash {get; set;}
-        public byte[] PasswordSalt {get; set;}
-        public DateTime DateOfBirth { get; set; }
+         public int Id { get; set; }
+        public string UserName { get; set; }   
+        public string PhotoUrl { get; set; }    
+        public int  Age { get; set; }
         public string KnownAs { get; set; }
         public DateTime Created { get; set; } = DateTime.Now;
         public DateTime LastActive { get; set; } = DateTime.Now;
@@ -21,9 +19,6 @@ namespace API.Entities
         public string Interests { get; set; }
         public string City { get; set; }
         public string Country { get; set; }
-        public ICollection<Photo> Photos { get; set; }
-        // public int getAge(){
-        //     return DateOfBirth.CalculateAge();
-        // }
+        public ICollection<PhotoDTO> Photos { get; set; }
     }
 }
